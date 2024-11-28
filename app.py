723d3a46eba22d570be887a19682e9fbf8032a9c
Swapnil shah <46062928+shahsapnil@users.noreply.github.com>
2024-11-28 17:16:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -32,24 +32,36 @@ def register():
             mainlist.append(list1)
         # print(mainlist)
         finallist=[]
-        passlist=mainlist[:6]
+        passlist=mainlist[:7]
         list2=[]
         for i in passlist:
             list2.append(i[1])
         # print(list2)
-        for i in range(6,len(mainlist)):
+        for i in range(7,len(mainlist)):
             if mainlist[i][1]!='':
                 finallist.append(mainlist[i][1])
         print(finallist)
-        print(list2)
-        total=(finallist[2]/1.28)*finallist[3]
+
+
+        def roundTraditional(val,digits):
+            return round(val+10**(-len(str(val))-1), digits)
+
+        rate=finallist[3]/1.28
+        total=(finallist[3]/1.28)*finallist[2]
+
+        total1=roundTraditional(total,2)
+
+        print(total1)
         cgst=(total*14)/100
+        cgst1=roundTraditional(cgst,2)
         sgst=(total*14)/100
+        sgst1=roundTraditional(sgst,2)
         final = total+cgst+sgst
         roffvalue=round(final)
         roff=round(final-roffvalue,2)
+        invoice=int(list2[1])
 
         textfinal=num2words(roffvalue).upper()+' ONLY'
-        return render_template('invoice.html',finallist=finallist,list2=list2,roffvalue=roffvalue,roff=roff,textfinal=textfinal)
+        return render_template('invoice.html',finallist=finallist,list2=list2,roffvalue=roffvalue,roff=roff,textfinal=textfinal,invoice=invoice,total=total1,rate=rate,cgst=cgst1,sgst=sgst1,key=list2[0])
 if __name__=="__main__":
     app.run(debug=True)
