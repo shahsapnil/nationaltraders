4d6a004d09e58a5b70d1795e79a24ee255f9b32b
Swapnil shah <46062928+shahsapnil@users.noreply.github.com>
2024-11-28 17:41:38 +0530

    test

--- app.py
+++ app.py
@@ -65,3 +65,6 @@ def register():
         return render_template('invoice.html',finallist=finallist,list2=list2,roffvalue=roffvalue,roff=roff,textfinal=textfinal,invoice=invoice,total=total1,rate=rate,cgst=cgst1,sgst=sgst1,key=list2[0])
 if __name__=="__main__":
     app.run(debug=True)
+
+    tesr
+    
